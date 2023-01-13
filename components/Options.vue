e5a4fd58e5c19433edee1977b10572403f7620a8
m1kx <mika.rottlaender@icloud.com>
2023-01-13 17:13:55 +0100

    fixed auto highlight

--- components/Options.vue
+++ components/Options.vue
@@ -14,6 +14,12 @@ export default {
   mounted() {
     this.courses = this.currentUser.subjects.replaceAll(":", ",");
     this.current = this.currentUser.class;
+    Object.keys(this.$refs).forEach(el => {
+      const button = this.$refs[el] as HTMLButtonElement;
+      if (button.innerHTML == this.current) {
+        button.classList.add('selected');
+      }
+    });
   },
   methods: {
     validCheck(): boolean {
@@ -113,8 +119,8 @@ export default {
       <label>Kurse</label>
       <input onkeyup="this.value = this.value.toUpperCase();" class="valid-check" v-model="courses" id="courses" type="text" placeholder="E G1,D G4,M L1,MU G1" pattern="^([A-Z]{1,2} [A-Z]\d)(,([A-Z]{1,2} [A-Z]\d))*$" required>
       <label>Stufe: {{ current }}</label>
-      <button class="select" @click="button_select($event)">EF</button><button class="select" @click="button_select($event)">Q1</button><button class="select" @click="button_select($event)">Q2</button><br>
-      <button @click="button_submit($event)" style="background-color: rgba(255, 255, 255, 0.162); margin-top: 35px; width: 50vw;">OK</button>
+      <button ref="select_button_1" class="select" @click="button_select($event)">EF</button><button ref="select_button_2" class="select" @click="button_select($event)">Q1</button><button ref="select_button_3" class="select" @click="button_select($event)">Q2</button><br>
+      <button @click="button_submit($event)" style="background-color: rgba(255, 255, 255, 0.162); margin-top: 35px; width: 50vw;">AKTUALISIEREN</button>
     </div>
   </div>
 </template>
