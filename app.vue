c79bce81366a4eb3a73736e89e8ea6c199e983c6
m1kx <mika.rottlaender@icloud.com>
2023-03-24 15:23:03 +0100

    ios home screen support

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
-<script>
+<script setup>
 import "./assets/style.css";
+
+const title = ref("LMG Bot")
+
+useHead({
+  title,
+  meta: [
+    { name: "apple-mobile-web-app-capable", content: "yes" },
+    { name: "apple-mobile-web-app-status-bar-style", content: "black-translucent" },
+    { name: "apple-mobile-web-app-title", content: "VTRBOT" }
+  ],
+  link: [
+    { rel: "apple-touch-icon", href: "icon.png" },
+    { rel: "apple-touch-startup-image", href: "splash.png" }
+  ]
+})
 </script>
 <template>
   <div>
