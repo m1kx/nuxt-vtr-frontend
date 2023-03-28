9b15e7a84bca28dc6199fef9348354f844dd3fb3
m1kx <mika.rottlaender@icloud.com>
2023-03-28 09:38:51 +0200

    fix

--- components/ScoreDisplay.vue
+++ components/ScoreDisplay.vue
@@ -10,9 +10,10 @@ export default {
   },
   async mounted() {
     const users: ListResult<any[]> = await pb.instance.collection("users").getFullList(200, {});
+    const items = users.items as Array<Object>;
     // @ts-ignore
-    users.items.sort((a, b) => (b.score + b.h_score) - (a.score + a.h_score));
-    this.list = users.items;
+    items.sort((a, b) => (b.score + b.h_score) - (a.score + a.h_score));
+    this.list = items as any[];
   }
 }
 </script>
